fcc0dc1fd9d35201c85252d1608e45732bddfc59
mozammel <mozammel1991h@gmail.com>
2024-08-23 09:50:51 +0400

    remove spaces from string

--- oop.php
+++ oop.php
@@ -59,6 +59,6 @@ $member2 = New member('Jane Smith');
 $member1->borrowBook($book1);
 $member2->borrowBook($book2);
 
-echo "Available Copies of  ' ".$book1->getTitle()."': ".$book1->getAvailableCopies()."<br/>";
-echo "Available Copies of  ' ".$book2->getTitle()."': ".$book2->getAvailableCopies()."<br/>";
+echo "Available Copies of '".$book1->getTitle()."': ".$book1->getAvailableCopies()."<br/>";
+echo "Available Copies of '".$book2->getTitle()."': ".$book2->getAvailableCopies()."<br/>";
 
